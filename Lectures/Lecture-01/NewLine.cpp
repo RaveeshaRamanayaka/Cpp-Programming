0be3a80f5127f33857ecf29850da21a1c20ba5dc
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-06 19:12:07 +0530

    Update NewLine.cpp

--- Lectures/Lecture-01/NewLine.cpp
+++ Lectures/Lecture-01/NewLine.cpp
@@ -1,1 +1,17 @@
+/* "iostream" is a header file that contains
+* functions for input/output operations ( cin and cout )
+*/
+#include <iostream>
 
+// main function
+int main(void) {
+	
+    // "cout" is a Output Statement
+    std::cout << "Name\nRaveesha Ramanayaka\n";
+    
+    // "endl" use to add new line
+    std::cout << "Name" << std::endl;
+    std::cout << "Raveesha Ramanayaka" << std::endl;
+    
+    return 0;
+}
