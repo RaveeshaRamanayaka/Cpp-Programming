95d02799508b925add564c0a3e8b6e5797bfa647
RaveeshaRamanayaka <86767879+RaveeshaRamanayaka@users.noreply.github.com>
2021-10-13 23:28:32 +0530

    Update Lab.h

--- OOC-Assignment-01/Past Papers/2020/Lab/Lab-S2/Lab.h
+++ OOC-Assignment-01/Past Papers/2020/Lab/Lab-S2/Lab.h
@@ -1,1 +1,11 @@
+class Lab {
+    private:
+        int labID;
+        int capacity;
 
+    public:
+        void setLabDetails(int ilabID, int icapacity);
+        int getCapacity();
+        int getLabID();
+
+};
